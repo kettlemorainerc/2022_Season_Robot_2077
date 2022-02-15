ec2cf764f76a8bbaf77d276eba60bbb49ff7aa0a
aj <aj@aj.com>
2022-02-14 18:45:45 -0600

    Commit for merge for both devices.

---
+++ src/main/java/org/usfirst/frc/team2077/commands/RunIntake.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 FRC Team 2077. All Rights Reserved.                     */
+/* Open Source Software - may be modified and shared by FRC teams.            */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2077.commands;
+
+import static org.usfirst.frc.team2077.Robot.robot_;
+
+public class LoadLauncher extends RepeatedCommand {
+  public LoadLauncher(){
+    addRequirements(robot_.newLauncher_);
+  }
+
+  @Override
+  public void initialize() {
+    robot_.newLauncher_.setRunning(true);
+  }
+
+  @Override
+  public void execute() {
+    robot_.newLauncher_.load();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    robot_.newLauncher_.stopLoader();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
